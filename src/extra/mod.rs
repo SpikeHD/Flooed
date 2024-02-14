717dfd70b08229d4656a2603846fcb68abe659f1
SpikeHD <spikegdofficial@gmail.com>
2024-02-14 10:35:56 -0800

    fix: scaffold themes and plugnis

---
+++ src/extra/mod.rs
@@ -0,0 +1,10 @@
+pub mod themes;
+pub mod plugins;
+
+pub fn register_plugin_commands(ws: &mut super::util::ws::WsConnector) {
+  // TODO
+}
+
+pub fn register_theme_commands(ws: &mut super::util::ws::WsConnector) {
+  // TODO
+}
