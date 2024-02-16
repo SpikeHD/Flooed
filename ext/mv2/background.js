2a69cad87c54c4f39d9b067b7c2ab634f25d28f2
SpikeHD <spikegdofficial@gmail.com>
2024-02-16 12:39:27 -0800

    fix: first-load csp writing

--- ext/mv2/background.js
+++ ext/mv2/background.js
@@ -9,3 +9,10 @@ chrome.webRequest.onHeadersReceived.addListener(
   { urls: ["*://*.discord.com/*", "*://discord.com/*"] },
   ["blocking", "responseHeaders"],
 );
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.query({ url: "*://discord.com/*" }, (tabs) => {
+    // Reload the page to apply the new CSP
+    tabs.forEach((tab) => chrome.tabs.reload(tab.id));
+  });
+});
